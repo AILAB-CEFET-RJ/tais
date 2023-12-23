70a9fa690b66d0cf732dee5f8bcf0ee09b434608
Lucas Farolfi <49098049+lucasfarolfi@users.noreply.github.com>
2023-12-23 17:33:15 -0300

    fix: entrypoint sh files from api

--- api/app/entrypoint_dev.sh
+++ api/app/entrypoint_dev.sh
@@ -1,10 +1,9 @@
-#!/usr/bin/env bash
+#!/bin/bash
 export DB_HOSTNAME=db
 export DB_USERNAME=root
 export DB_PASSWORD=root
 export DB_PORT=5432
 export DB_NAME=ais
-
-mv init.sql /docker-entrypoint-initdb.d/
-
+export FLASK_APP=app # api filename
+mv init.sql /docker-entrypoint-initdb.d/ # Init migration sql file
 python3 -m debugpy --listen "0.0.0.0:5678" -m flask run -h 0.0.0.0 -p 5000
