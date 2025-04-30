ee4ebc61b1dfadd58a33ab7ecec59d5ac9013e2d
Gabriel-gag <gabrielalvesgiuliano@gmail.com>
2025-04-29 21:11:48 -0300

    Fix
    Troca de bibliotecas

--- api/app/routes/visualization.py
+++ api/app/routes/visualization.py
@@ -6,7 +6,8 @@ import io
 from flask import Response
 from routes.routesmap import get_routesmap_from_csv
 from flask import Blueprint, request, Response
-from mpl_toolkits.basemap import Basemap
+import cartopy.crs as ccrs
+import cartopy.feature as cfeature
 import numpy as np
 from random import random
 import colorsys
@@ -22,15 +23,13 @@ def view_routesmap() -> Response:
     coordinates = list((tuple(c) for c in data["coordinates"]))
     if coordinates is None or len(coordinates) == 0:
         return Response(f"Erro: {data['error']}", status=400)
-    
+
     routes = {}
-
     for line in coordinates:
-        if list(routes.keys()).count(line[2])==1:
-            routes[line[2]].append((line[0],line[1]))
+        if line[2] in routes:
+            routes[line[2]].append((line[0], line[1]))
         else:
-            routes[line[2]]=[(line[0],line[1])]
-
+            routes[line[2]] = [(line[0], line[1])]
     # Verificar limites
     lat_min = data["min_latitude"]
     lat_max = data["max_latitude"]
@@ -53,50 +52,42 @@ def view_routesmap() -> Response:
             assert lat_min < lat_max and lon_min < lon_max, "Coordenadas da bounding box inválidas"
         except (ValueError, AssertionError) as e:
             return Response(f"Erro no formato da bounding box: {str(e)}", status=400)
-        print(f"Lat_Min: {lat_min}, Lon_Min: {lon_min}, Lat_Max: {lat_max}, Lon_Max: {lon_max}")
         lat_padding, lon_padding = 0, 0
-        
     else:
         # Calcular padding adequado (ajustado)
-        if abs((lat_max - lat_min))>abs((lon_max - lon_min)):
-                lat_padding = max((lat_max - lat_min),0)/4
-                lon_padding = lat_padding
+        if abs((lat_max - lat_min)) > abs((lon_max - lon_min)):
+            lat_padding = max((lat_max - lat_min), 0) / 4
+            lon_padding = lat_padding
         else:
-            lon_padding = max((lon_max - lon_min),0)/4
+            lon_padding = max((lon_max - lon_min), 0) / 4
             lat_padding = lon_padding
 
     # Criação do routesmap
     fig = plt.figure(figsize=(10, 10))
     try:
         # criando recorte contendo rota encontrada, com no mínimo 9 graus em latitude e longitude, do mapa mundi usando projeção cilindrica de Miller
-        mapamundi = Basemap(
-            projection="mill",
-            llcrnrlat=lat_min - lat_padding,
-            llcrnrlon=lon_min - lon_padding,
-            urcrnrlat=lat_max + lat_padding,
-            urcrnrlon=lon_max + lon_padding,
-            resolution='f'
-        )
-        mapamundi.drawcoastlines(linewidth=1)
-        mapamundi.drawcountries(linewidth=3)
-        mapamundi.drawstates(linestyle="dashed")
-        mapamundi.fillcontinents(color='lightgreen', lake_color='blue')
-        mapamundi.drawmapboundary(fill_color="blue")
+        ax = plt.axes(projection=ccrs.Miller())
+        ax.set_extent([lon_min - lon_padding, lon_max + lon_padding,
+                       lat_min - lat_padding, lat_max + lat_padding], crs=ccrs.PlateCarree())
 
+        ax.add_feature(cfeature.LAND, facecolor='lightgreen')
+        ax.add_feature(cfeature.OCEAN, facecolor='blue')
+        ax.add_feature(cfeature.COASTLINE, linewidth=1)
+        ax.add_feature(cfeature.BORDERS, linewidth=3)
+        ax.add_feature(cfeature.STATES, linestyle='--')
         # Sobrepor scatterplot de cada embarcação ao mapa
         for id, coords in routes.items():
             # 0.5 a 0.7 é hue de azul
-            color = [random(),random(),random()]
+            color = [random(), random(), random()]
             while color[0] >= 0.5 and color[0] < 0.7:
-                color[0]=random()
+                color[0] = random()
             while color[1] < 0.7:
-                color[1]=random()
+                color[1] = random()
             while color[2] < 0.5:
-                color[2]=random()
+                color[2] = random()
             color = colorsys.hsv_to_rgb(*color)
             lats, lons = zip(*coords)
-            x, y = mapamundi(lons, lats)
-            mapamundi.plot(x,y,color=color, marker=None,zorder=5,lw=1)
+            ax.plot(lons, lats, color=color, transform=ccrs.PlateCarree(), linewidth=1, zorder=5)
 
         plt.title("Mapa de Rotas de Embarcação", fontsize=24)
         plt.xlabel("Longitude", fontsize=20)
@@ -105,12 +96,12 @@ def view_routesmap() -> Response:
         os.makedirs(IMAGE_SAVE_DIR, exist_ok=True)
         image_path = os.path.join(IMAGE_SAVE_DIR, "routes.svg")
         plt.savefig(image_path, format="svg")
-
         # Gera a imagem em memória para resposta HTTP
         img = io.BytesIO()
         plt.savefig(img, format="svg")
         img.seek(0)
     finally:
         plt.close(fig)  # Garante que o recurso será liberado
+
 
     return Response(img.getvalue(), mimetype="image/svg+xml")
